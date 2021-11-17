2a77581e22b36a09467d6c5a35d17ff547d0344a
Octavio Lara <59698374+olaracode@users.noreply.github.com>
2021-11-17 18:37:09 +0000

    command created.

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ from replit import db
 cg = CoinGeckoAPI()
 client = discord.Client()
 
-bitcoin = cg.get_price(ids='bitcoin', vs_currencies='usd')
-print(bitcoin['bitcoin']['usd'])
+favourites = ["bitcoin", "ethereum", "litecoin"]
+
 if "active" not in db.keys():
   db["active"] = True
 
+#Gets the current price of a crypto currency. Takes the name of the crypto and the name of the pair
 def get_coin(ids, vs_currencies):
   current_price = cg.get_price(ids, vs_currencies)
   return current_price[ids][vs_currencies]
 
+
+def get_favourites(favourites, pair):
+  favourite_prices = []
+  for coin in favourites:
+      coin_price = get_coin(coin, pair)
+      favourite_prices.append([coin, coin_price])
+  return favourite_prices
 
 @client.event
 async def on_ready():
@@ -32,15 +40,22 @@ async def on_message(message):
   msg = message.content
   
   #get price
+
+  # if msg.startswith("$")
+
   if msg.startswith("$vs"):
-    value = msg.split("$vs", 1)[1]
+    value = msg.split("$vs ", 1)[1]
     [token, vs_currencies] = value.split()
     price = get_coin(token, vs_currencies)
     await message.channel.send("Current {} price is {} {}".format(token, price, vs_currencies))
   
-  #list available pairs
-  #if msg.startswith("$list"):
-   # null
+  #Get favourites price
+  if msg.startswith("$fav"):
+    value = msg.split("$fav ", 1)[1]
+
+    favourite_values = get_favourites(favourites, value)
+    for favourite_value in favourite_values:
+      await message.channel.send("Current {} price is {} {}".format(favourite_value[0], favourite_value[1], value))
 
   #Turn on or of
   if msg.startswith("$trady"):
